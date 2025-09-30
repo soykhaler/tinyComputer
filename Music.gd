103056514f90540b6ef64f77c2da41e279f55c55
SoyKhaler <aaronstone2000@gmail.com>
2025-09-30 13:07:19 +0200

    piano added

--- Music.gd
+++ Music.gd
@@ -1,6 +1,19 @@
 extends Control
-
-
-
-func _ready():
-	pass 
+func do():
+	$"%sound".stream = preload("res://sounds/do.ogg")
+	$"%sound".play()
+func re():
+	$"%sound".stream = preload("res://sounds/re.ogg")
+	$"%sound".play()
+func mi():
+	$"%sound".stream = preload("res://sounds/mi.ogg")
+	$"%sound".play()
+func fa():
+	$"%sound".stream = preload("res://sounds/fa.ogg")
+	$"%sound".play()
+func sol():
+	$"%sound".stream = preload("res://sounds/sol.ogg")
+	$"%sound".play()
+func la():
+	$"%sound".stream = preload("res://sounds/la.ogg")
+	$"%sound".play()
