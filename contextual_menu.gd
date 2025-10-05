004c0ef95e422a46995b9b650c8eaefd97e896e6
SoyKhaler <aaronstone2000@gmail.com>
2025-10-05 19:11:17 +0200

    contextual menu show is possible now

--- contextual_menu.gd
+++ contextual_menu.gd
@@ -2,4 +2,15 @@ extends Control
 
 
 func _ready():
-	pass 
+	hideMenu()
+
+func _process(delta):
+	if Input.is_action_just_pressed("RClick"):
+		showMenu()
+
+
+func showMenu():
+	$VBoxContainer.visible = true
+	
+func hideMenu():
+	$VBoxContainer.visible = false
