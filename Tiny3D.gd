9f979644e08d30c36db0624ba4c3bc16c4253bb8
SoyKhaler <aaronstone2000@gmail.com>
2025-10-05 19:36:35 +0200

    cube menu working

--- Tiny3D.gd
+++ Tiny3D.gd
@@ -1,6 +1,4 @@
 extends Spatial
-
-
-
 func Close():
 	get_tree().change_scene("res://main.tscn")
+
--- contextual_menu.gd
+++ contextual_menu.gd
@@ -9,7 +9,15 @@ func showMenu():
 	$VBoxContainer.visible = true
 func hideMenu():
 	$VBoxContainer.visible = false
-
-
 func cube():
+	var cube_mesh = preload("res://models/cube_mesh.tscn")
+	var cube_instance = cube_mesh.instance()
+	add_child(cube_instance)
+	cube_instance.owner = get_tree().get_current_scene()
+	hideMenu()
+func sphere():
+	pass # Replace with function body.
+func plane():
+	pass # Replace with function body.
+func cylinder():
 	pass # Replace with function body.
