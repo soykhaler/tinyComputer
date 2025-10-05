3b21a6f7729548824ba5077faab423a374d6ede8
SoyKhaler <aaronstone2000@gmail.com>
2025-10-05 19:29:41 +0200

    working menu with no buttons still yet

--- contextual_menu.gd
+++ contextual_menu.gd
@@ -1,16 +1,15 @@
-extends Control
-
-
+extends Node2D
 func _ready():
 	hideMenu()
-
 func _process(delta):
-	if Input.is_action_just_pressed("RClick"):
+	if Input.is_action_just_pressed("menu"):
+		position = get_global_mouse_position()
 		showMenu()
-
-
 func showMenu():
 	$VBoxContainer.visible = true
-	
 func hideMenu():
 	$VBoxContainer.visible = false
+
+
+func cube():
+	pass # Replace with function body.
